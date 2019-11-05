3ea80c1f18f8c018a0d1f384a5d3ef2716987889
cthomas502 <coreythomas14@gmail.com>
2019-11-05 16:48:59 -0500

    Randomizer Working 1

--- src/RandomAnimal.js
+++ src/RandomAnimal.js
@@ -2,92 +2,120 @@ import React from 'react';
 import axios from 'axios';
 
 class RandomAnimal extends React.Component {
-    constructor(props) {
+    constructor() {
         super()
         this.state = {
+            token: '',
             newAnimalArr: [],
-            image: [],
+            // image: [],
             randomLog: 30,
             randomAnimal: '',
-            gotAnimal: false
+            gotAnimal: false,
         }
     }
     componentDidMount() {
-        this.callAxios()
+        this.authentication()
+    }
+    authentication() {
+        axios({
+            method: 'post',
+            url: 'https://api.petfinder.com/v2/oauth2/token',
+            data: "grant_type=client_credentials&client_id=lWSjPsZx1WhEQdoWOU9N2luD4gfXlBjySDbKD9qEK3cyjoJfAP&client_secret=TvYHcRREzNh60sZCpcpl2f0HsKgAJ59kUivd1KIt"
+        })
+            .then(response => {
+                this.setState({ token: response.data.access_token })
+                if (this.state.token) { this.callAxios() }
+            })
     }
     callAxios() {
-        // axios({
-        //     method: 'post',
-        //     url: 'https://api.petfinder.com/v2/oauth2/token',
-        //     data: "grant_type=client_credentials&client_id=lWSjPsZx1WhEQdoWOU9N2luD4gfXlBjySDbKD9qEK3cyjoJfAP&client_secret=TvYHcRREzNh60sZCpcpl2f0HsKgAJ59kUivd1KIt"
-        //   })
-        //   .then(response => {
-        //     console.log(response.data)
-        //   })
         axios({
             method: 'get',
             url: 'https://api.petfinder.com/v2/animals',
             headers: {
-                'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQwOTNlYmQ4NzY2MDc5MWNiOGMyMzY3MDY4OTBjZGVjYWQ3YTlmYmFhYzZjOTQzNTcxZjE0OTE5NTc4YmYxZDMzNTRjYTQxNGIwZjk0MTM4In0.eyJhdWQiOiJsV1NqUHNaeDFXaEVRZG9XT1U5TjJsdUQ0Z2ZYbEJqeVNEYktEOXFFSzNjeWpvSmZBUCIsImp0aSI6ImQwOTNlYmQ4NzY2MDc5MWNiOGMyMzY3MDY4OTBjZGVjYWQ3YTlmYmFhYzZjOTQzNTcxZjE0OTE5NTc4YmYxZDMzNTRjYTQxNGIwZjk0MTM4IiwiaWF0IjoxNTcyOTY1ODg0LCJuYmYiOjE1NzI5NjU4ODQsImV4cCI6MTU3Mjk2OTQ4NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.l8QxLLQqAHtbpoKG1Fa0vZIaxKC9eND-1cU63rQV71Td2eoFqiJ7tHtdDgj0JF5AmH5IGohyz5T4_lu-_kg2EHy2VXMKYGr17MaT_VEG2nV6fXAvLIqQy-92hxh0itjy2gwlmlvxtporCl66b2oUgSo4EdhoZK6sYllq-3RXOSzdX2iX7X28b72VGnhcVbBOxHYPhQtG7mBM7OrzBj__Sje309TVM_hZEtIP7sKETM_MHFJhFiqJ4ersScMSqIJ3pG7e3XpGhVImpWriX9T19BsFS4vbGOXz4sNDlv6PadjRKBmsehqO8pDIltTSPBsxihhOuMIItVOVh2gee2TX3g"
+                'Authorization': `Bearer ${this.state.token}`
             }
 
         })
             .then(response => {
-                let animalArr = response.data.animals
-                let midArr = animalArr.filter(animal => animal.status === "adoptable")
-                return midArr
+                // let animalArr = response.data.animals
+                // let midArr = animalArr.filter(animal => animal.status === "adoptable")
+                // return midArr
+                return response.data.animals.filter(animal => animal.status === "adoptable")
             })
             .then(midArr => {
                 console.log(midArr)
                 this.setState({
                     newAnimalArr: midArr
                 })
-                console.log(this.state.newAnimalArr)
-                if (this.state.newAnimalArr.length !== 0) {
-                    let randomGen = Math.floor(Math.random() * this.state.newAnimalArr.length)
-                    this.setState({
-                        randomLog: randomGen
-                    })
-                    if ((this.state.randomLog !== 30) && (this.state.gotAnimal === false)) {
-                        this.setState({
-                            randomAnimal: this.state.newAnimalArr[this.state.randomLog],
-                            gotAnimal: true
-                        })
-                    }
-                    console.log(this.state.randomAnimal)
-                    this.imageOutput()
-                }
+            })
+            .then(res => {
+                console.log(this.state)
+                this.allOutput()
             })
     }
-    imageOutput() {
-        if (this.state.randomAnimal.photos) {
-            const images = this.state.randomAnimal.photos.map((photo, i) => {
-                return <img key={i} src={photo.medium} alt='' />
+    allOutput() {
+        console.log(this.state.newAnimalArr)
+        if (this.state.newAnimalArr.length !== 0) {
+            let randomGen = Math.floor(Math.random() * this.state.newAnimalArr.length)
+            this.setState({ randomLog: randomGen }, () => {
+                console.log(this.state.randomLog)
+                if ((this.state.randomLog !== 30) && (!this.state.gotAnimal)) {
+                    let copiedArr = this.state.newAnimalArr.slice()
+                    copiedArr.splice(this.state.randomLog, 1)
+
+                    this.setState((state, props) => ({
+                        randomAnimal: state.newAnimalArr[state.randomLog],
+                        gotAnimal: true,
+                        newAnimalArr: copiedArr
+                    }))
+
+                }
             })
-            this.setState({
-                image: images
-            })
+            console.log(this.state.randomAnimal)
+
         }
+
+
+        // if (this.state.randomAnimal.photos) {
+        //     const images = this.state.randomAnimal.photos.map((photo) => {
+        //         return photo.medium
+        //     })
+        //     this.setState({ image: images[0] }, () => { images.shift() })
+            
+        //     // Add functionality to change images
+        // } else {
+        //     this.setState({ image: [] })
+        // }
     }
     newAnimal = () => {
         this.setState({
-            image: []
-        })
+            gotAnimal: false,
+        }, this.allOutput())
     }
     render() {
-        // let randomLog = (this.state.newAnimalArr.length !== 0) ? Math.floor(Math.random() * this.state.newAnimalArr.length) : 0
-        // let display = (this.state.newAnimalArr !== 0) ? 'randomAnimal.name' : 'none'
+        console.log(this.state)
         return (
             <div>
                 {((this.state.newAnimalArr.length !== 0) && (this.state.randomAnimal)) ?
                     <div>
                         <h1>{this.state.randomAnimal.name}</h1>
-                        <h2>{this.state.randomAnimal.gender} {this.state.randomAnimal.breeds.primary}</h2>
-                        <div className='allImages'>{this.state.image}</div>
-
-                        {/* <button onClick={this.newAnimal}>New Animal</button> */}
+                        <h2>{this.state.randomAnimal.gender} {this.state.randomAnimal.species}</h2>
+                        <h2>{this.state.randomAnimal.colors.primary} {this.state.randomAnimal.age} {this.state.randomAnimal.breeds.primary}</h2>
+                        {(this.state.randomAnimal.photos) ? <img src={this.state.randomAnimal.photos[0].medium} alt='No pictures listed'/> : 'No Photo Available'}
+                        <h3>{this.state.randomAnimal.contact.address.city} {this.state.randomAnimal.contact.address.state}</h3>
+                        <a href={this.state.randomAnimal.url}>Interested? Learn more here!</a>
+                        <h3>House Trained: {(this.state.randomAnimal.attributes.house_trained) ? 'Yes' : 'No'}</h3>
+                        <h3>Needs shots: {(this.state.randomAnimal.attributes.shots_current) ? 'No' : 'Yes'}</h3>
+                        <h3>Spayed/Neutered: {(this.state.randomAnimal.attributes.spayed_neutered) ? 'Yes' : 'No'}</h3>
+                        <h3>Special Needs: {(this.state.randomAnimal.attributes.special_needs) ? 'Yes' : 'No'}</h3>
+                        {(this.state.randomAnimal.attributes.declawed !== null) ? <h3>Declawed: {(this.state.randomAnimal.attributes.declawed) ? 'Yes' : 'No'}</h3> : ''}
+                        <button onClick={this.newAnimal}>New Animal</button>
                     </div>
-                    : 'Loading...'}
+                    :
+                    <div>
+                        <h2>Loading...</h2>
+                        <p>If this takes more than a minute, try refreshing the page</p>
+                    </div>}
             </div>
         )
     }
